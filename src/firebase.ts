837a7ac5d405603a1129a90d9afa9f4fa940c1fa
N A F I <mdannafi548@gmail.com>
2026-05-04 14:12:49 +0600

    Cloudinary settings updated

--- src/firebase.ts
+++ src/firebase.ts
@@ -23,3 +23,17 @@ const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export default app;
+
+export const uploadToCloudinary = async (file: File) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', 'dubhub_preset');
+
+  const response = await fetch('https://api.cloudinary.com/v1_1/dxnp0qmon/upload', {
+    method: 'POST',
+    body: formData,
+  });
+
+  const data = await response.json();
+  return data.secure_url;
+};
